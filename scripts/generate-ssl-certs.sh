021c1be65dfb4843cc295fed0cd73d5dc6b42924
Alexander Bleissem <alexander@bleissem.com>
2017-08-22 02:43:57 +0200

    use dns2.bleissem.com

--- scripts/generate-ssl-certs.sh
+++ scripts/generate-ssl-certs.sh
@@ -11,7 +11,7 @@ fi
 echo "Generating self-signed certificates..."
 mkdir -p /config/sslcerts
 openssl genrsa -out ./config/sslcerts/key.pem 1024
-openssl req -new -subj "/C=/ST=/L=/O=/CN=lala" -key ./config/sslcerts/key.pem -out ./config/sslcerts/csr.pem
+openssl req -new -subj "/C=/ST=/L=/O=/CN=dns2.bleissem.com" -key ./config/sslcerts/key.pem -out ./config/sslcerts/csr.pem
 openssl x509 -req -days 9999 -in ./config/sslcerts/csr.pem -signkey ./config/sslcerts/key.pem -out ./config/sslcerts/cert.pem
 rm ./config/sslcerts/csr.pem
 chmod 600 ./config/sslcerts/key.pem ./config/sslcerts/cert.pem
